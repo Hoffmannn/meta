82816dd0cb67b42c8286e13a07ad63fe3b1279a1
Lucas <lucashoffmann99@gmail.com>
2021-12-06 20:40:48 -0300

    Criando rota Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import CountryList from "../../components/CountryList";
+import React from "react";
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <CountryList />
+    </div>
+  );
+};
+
+export default Home;
