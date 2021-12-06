d3ca6c0b49dec713386cb4a064927c8ef071faff
Lucas <lucashoffmann99@gmail.com>
2021-12-06 20:47:53 -0300

    Iniciando paginação na lista de países

--- src/components/CountryList/index.tsx
+++ src/components/CountryList/index.tsx
@@ -8,8 +8,8 @@ import CountryCard from "../CountryCard";
 const CountryList: React.FC = () => {
   const [page, setPage] = useState(0);
   const COUNTRIES_QUERY = gql`
-    query {
-      Country(first: 10, offset: 0) {
+    query countriesQuery($page: Int) {
+      Country(first: 5, offset: $page) {
         _id
         name
         capital
@@ -19,9 +19,32 @@ const CountryList: React.FC = () => {
       }
     }
   `;
-  const { loading, error, data, fetchMore } = useQuery(COUNTRIES_QUERY);
+  const { loading, error, data, fetchMore } = useQuery(COUNTRIES_QUERY, {
+    variables: { page: page },
+  });
 
   const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", loadMore);
+  }, []);
+
+  function loadMore() {
+    if (
+      document.scrollingElement &&
+      window.innerHeight + document.documentElement.scrollTop ===
+        document.scrollingElement.scrollHeight
+    ) {
+      // setPage((previousPage) => previousPage + 1);
+      fetchMore({
+        variables: { page: page * 5 },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          console.log(previousResult);
+          console.log(fetchMoreResult);
+        },
+      });
+    }
+  }
 
   React.useEffect(() => {
     data && setCountries((oldValues): any => [...oldValues, ...data.Country]);
