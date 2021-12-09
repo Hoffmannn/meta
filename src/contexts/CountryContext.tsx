f2f35f491106f6f64a38a418a5e19812dc1c398c
Lucas Hoffmann <lucashoffmann@gmail.com>
2021-12-09 11:36:46 -0300

    Ordenando lista de países por nome traduzido

--- src/contexts/CountryContext.tsx
+++ src/contexts/CountryContext.tsx
@@ -2,17 +2,33 @@ import React, { createContext, useEffect, useState } from "react";
 
 import api from "../api";
 
+export type countryData = {
+  name: {
+    common: string;
+  };
+  flag: string;
+  capital: string[];
+  area: number;
+  population: number;
+  tld: string[];
+  translations: {
+    por: {
+      common: string;
+    };
+  };
+};
+
 type CountryContextProps = {
   isLoading: boolean;
   setIsLoading: (boolean: boolean) => void;
-  countries: any[];
+  countries: countryData[];
   setCountries: (countries: any) => void;
 };
 
 export const CountryContext = createContext({} as CountryContextProps);
 
 export const CountryProvider: React.FC = ({ children }) => {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([] as countryData[]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -20,10 +36,9 @@ export const CountryProvider: React.FC = ({ children }) => {
       api.get("/all").then((res) => {
         setCountries(
           res.data.sort((a: any, b: any) =>
-            a.name.common.localeCompare(b.name.common)
+            a.translations.por.common.localeCompare(b.translations.por.common)
           )
         );
-        console.log(res.data);
         setIsLoading(false);
       });
     } catch (error) {
