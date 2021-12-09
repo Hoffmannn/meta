4f7770d0745a0fa7a9fcbbfeddb10d1c9d453713
Lucas Hoffmann <lucashoffmann@gmail.com>
2021-12-09 11:40:26 -0300

    Edição de dados localmente

--- src/pages/home/country/index.tsx
+++ src/pages/home/country/index.tsx
@@ -1,42 +1,131 @@
+import { CountryContext, countryData } from "../../../contexts/CountryContext";
 import { Link, useLocation } from "react-router-dom";
-import React, { useContext, useEffect } from "react";
-import { gql, useQuery as useApolloQuery } from "@apollo/client";
-
-import { CountryContext } from "../../../contexts/CountryContext";
+import React, { useContext, useEffect, useState } from "react";
 
 const Country: React.FC = () => {
-  const { countries, isLoading } = useContext(CountryContext);
-  //Função para buscar query da URL
+  const { countries, setCountries } = useContext(CountryContext);
+  const [isDataEditable, setIsDataEditable] = useState(false);
+  const [country, setCountry] = useState({} as countryData);
+  const [countryTranslatedName, setCountryTranslatedName] = useState("");
+  const [capital, setCapital] = useState("");
+  const [area, setArea] = useState(0);
+  const [population, setPopulation] = useState(0);
+  const [tld, setTld] = useState("");
+
+  const query = useQuery();
+  //Buscar query da URL e preencher os dados do país
   function useQuery() {
     const { search } = useLocation();
 
     return React.useMemo(() => new URLSearchParams(search), [search]);
   }
-  const query = useQuery();
-  const countryName = query.get("name");
 
-  const country = countries.find(
-    (country) => country.name.common == countryName
-  );
+  useEffect(() => {
+    const countryName = query.get("name");
+    countries &&
+      setCountry(
+        countries.find((country) => country.name.common == countryName) ||
+          country
+      );
+  }, [countries]);
+
+  useEffect(() => {
+    if (country) {
+      country.translations &&
+        setCountryTranslatedName(country.translations.por.common);
+      country.capital && setCapital(country.capital[0]);
+      country.population && setPopulation(country.population);
+      country.area && setArea(country.area);
+      country.tld && setTld(country.tld[0]);
+    }
+  }, [country]);
+  //
+
+  const handleSaveNewData = () => {
+    const countryIndex = countries.findIndex((c) => c.name == country.name);
+    const newCountries = countries;
+    newCountries[countryIndex] = {
+      ...countries[countryIndex],
+      area: area,
+      capital: [capital],
+      population: population,
+      tld: [tld],
+      translations: { por: { common: countryTranslatedName } },
+    };
+
+    setCountries(newCountries);
+
+    setIsDataEditable(false);
+  };
 
   return (
     <>
       <Link to="/">
         <button>Voltar</button>
       </Link>
-      {country ? (
-        <div>
-          {/* Nome, bandeira, capital, área, população e top-level */}
-          <h2>
-            País - {country.translations.por.common} {country.flag}
-          </h2>
-          <h2>
-            Capital - {country.capital ? country.capital[0] : "Sem capital"}
-          </h2>
-          <h2>Área - {country.area}</h2>
-          <h2>População - {country.population}</h2>
-          <h2>Top Level Domain - {country.tld[0]}</h2>
-        </div>
+      {!isDataEditable ? (
+        <button onClick={() => setIsDataEditable(true)}>Editar dados</button>
+      ) : (
+        <button onClick={() => handleSaveNewData()}>Salvar dados</button>
+      )}
+
+      {Object.keys(country).length > 0 ? (
+        <>
+          {!isDataEditable ? (
+            <div>
+              <h2>
+                País: {countryTranslatedName} {country.flag}
+              </h2>
+              <h2>Capital: {country.capital ? capital : "Sem capital"}</h2>
+              <h2>Área (km²): {area}</h2>
+              <h2>População: {population}</h2>
+              <h2>Top Level Domain: {tld}</h2>
+            </div>
+          ) : (
+            <div>
+              <h2>
+                País:{" "}
+                <input
+                  type="text"
+                  value={countryTranslatedName}
+                  onChange={(e) => setCountryTranslatedName(e.target.value)}
+                />
+              </h2>
+              <h2>
+                Capital:{" "}
+                <input
+                  type="text"
+                  value={capital}
+                  onChange={(e) => setCapital(e.target.value)}
+                />
+              </h2>
+              <h2>
+                Área (km²):{" "}
+                <input
+                  type="number"
+                  value={area}
+                  onChange={(e) => setArea(parseInt(e.target.value))}
+                />
+              </h2>
+              <h2>
+                População:{" "}
+                <input
+                  type="number"
+                  value={population}
+                  onChange={(e) => setPopulation(parseInt(e.target.value))}
+                />
+              </h2>
+              <h2>
+                Top Level Domain:{" "}
+                <input
+                  type="text"
+                  value={tld}
+                  onChange={(e) => setTld(e.target.value)}
+                />
+              </h2>
+            </div>
+          )}
+        </>
       ) : (
         <h2>Carregando...</h2>
       )}
